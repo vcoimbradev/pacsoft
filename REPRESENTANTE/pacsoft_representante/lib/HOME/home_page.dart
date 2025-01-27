61bdc99f5e32cbe392c1432f1dc32b342560e148
Cristian Guedes <cristian.edel@yahoo.com>
2025-01-27 18:21:34 -0300

    Inclusao da logo e fonte

--- REPRESENTANTE/pacsoft_representante/lib/HOME/home_page.dart
+++ REPRESENTANTE/pacsoft_representante/lib/HOME/home_page.dart
@@ -12,16 +12,26 @@ class homepagestage extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("LOGIN", selectionColor: Colors.white),
-        backgroundColor: Color(0x7FA653),
+        title: Text("BAGSOFT", selectionColor: Colors.black, 
+        
+        style: TextStyle(
+          fontFamily: 'Etno',
+          fontWeight: FontWeight.w300,
+        ),
+        
+        ),
+        backgroundColor: Colors.green[200],
       ),
+
       body:Center(
         child: Container(
         height: double.infinity,
         width: double.infinity,
+        color: const Color.fromARGB(255, 250, 239, 233),
         child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children:[
+        Image.asset('assets/images/logo.jpg'),
         botao()]
       ),
       )
