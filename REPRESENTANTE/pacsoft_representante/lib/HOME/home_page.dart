542338f610dc801aa1e8bbc7468668f5a91ff245
Cristian Guedes <cristian.edel@yahoo.com>
2025-01-28 21:01:09 -0300

    Finalizado a tela de Login

--- REPRESENTANTE/pacsoft_representante/lib/HOME/home_page.dart
+++ REPRESENTANTE/pacsoft_representante/lib/HOME/home_page.dart
@@ -29,9 +29,38 @@ class homepagestage extends State<HomePage> {
         width: double.infinity,
         color: const Color.fromARGB(255, 250, 239, 233),
         child: Column(
+        
         mainAxisAlignment: MainAxisAlignment.center,
         children:[
-        Image.asset('assets/images/logo.jpg'),
+        Image.asset('assets/images/logo.png', height: 250, width: 200),
+        Text('CPF OU CNPJ', style: TextStyle(fontSize: 18),),
+        SizedBox(
+          width: 350,
+          child:TextField(
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            hintText: 'Digite seu CNPJ ou CPF',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100)
+            ),
+          ),
+        ),
+        ),
+        SizedBox(height: 10,),
+        Text('SENHA', style: TextStyle(fontSize: 18),),
+        SizedBox(
+          width: 350,
+          child:TextField(
+          textAlign: TextAlign.center,
+          decoration: InputDecoration(
+            hintText: 'Digite sua senha',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(100)
+            ),
+          ),
+        ),
+        ),
+        SizedBox(height: 10,),
         botao()]
       ),
       )
