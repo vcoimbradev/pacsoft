24f8a84173424f52f1809f533382a184c39b10e9
Cristian Guedes <cristian.edel@yahoo.com>
2025-02-07 17:31:31 -0300

    Início da pagina inicial

--- REPRESENTANTE/pacsoft_representante/lib/Components/componentes.dart
+++ REPRESENTANTE/pacsoft_representante/lib/Components/componentes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pacsoft_representante/HOME/LOGIN.dart';
+import 'package:pacsoft_representante/HOME/PG_INICIAL.dart';
 
 class Pacsoft extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      home: PgInicial(),
+      
     );
   }
 }
--- REPRESENTANTE/pacsoft_representante/lib/HOME/PG_INICIAL.dart
+++ REPRESENTANTE/pacsoft_representante/lib/HOME/PG_INICIAL.dart
@@ -14,16 +14,61 @@ class pginicialstage extends State<PgInicial>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("BAGSOFT", selectionColor: Colors.black, 
-        
-        style: TextStyle(
-          fontFamily: 'Etno',
-          fontWeight: FontWeight.w300,
-        ),
-        ),
+      appBar: AppBar( //BARRA SUPERIOR
+        titleSpacing: 30,
+        toolbarHeight: 100,
+        title: Image.asset('assets/images/logo.png', height: 60),
+        actions: <Widget>[
+          Padding(padding: EdgeInsets.only(right: 30),
+          child:  SizedBox(
+            child: IconButton(onPressed:(){}, icon: Icon(Icons.account_circle_outlined,size: 40,)),
+          )
+        )
+        ],
         backgroundColor: Colors.green[200],
       ),
+
+      body: Container( //MENU DE ATALHO
+        child: Align(
+          alignment: Alignment(0, -1),
+          
+        child: Column(
+
+          children: [
+            Padding(padding: EdgeInsets.only(top: 5)),
+
+            Text("Bem Vindo, !"),
+            Text("Atalhos"),
+            Text("Últimos pedidos"),
+            Text("Pedidos pendentes"),
+            Text("Preços"),
+            Text("Comissão"),
+            
+
+            Expanded( //LINHA DEBAIXO
+              child: Column(
+              children: [
+              Padding(padding: EdgeInsets.only(top: 5)),
+                Divider(thickness: 2,color: Colors.black,),
+                Text('Resumo do mês'),
+                Text('Total de pedidos'),
+                Text('25'),
+                Text('Pedidos em aberto'),
+                Text('12'),
+                Text('Pedidos entregues'),
+                Text('13'),
+              ],
+            )),
+          ],
+        ),
+      ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+        NavigationDestination(icon: Icon(Icons.home), label: 'teste'),
+        NavigationDestination(icon: Icon(Icons.home), label: 'teste'),
+      ]) ,
+
     );
   }
 
