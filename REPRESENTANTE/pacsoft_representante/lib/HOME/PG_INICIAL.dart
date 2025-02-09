c8c0122901e2f13170bd8201baaa52acbda6c2ed
Cristian Guedes <cristian.edel@yahoo.com>
2025-02-08 22:03:46 -0300

    Feita Mudanças nos numeros e coloquei a font Lato

--- REPRESENTANTE/pacsoft_representante/lib/HOME/PG_INICIAL.dart
+++ REPRESENTANTE/pacsoft_representante/lib/HOME/PG_INICIAL.dart
@@ -37,36 +37,62 @@ class pginicialstage extends State<PgInicial>{
           children: [
             Padding(padding: EdgeInsets.only(top: 5)),
 
-            Text("Bem Vindo, !"),
-            Text("Atalhos"),
-            Text("Últimos pedidos"),
-            Text("Pedidos pendentes"),
-            Text("Preços"),
-            Text("Comissão"),
+            Text("Bem Vindo, !", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w300, fontSize: 20),),
+            Text("Atalhos", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w800, fontSize: 20),),
+            Text("Últimos pedidos", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),),
+            Text("Pedidos pendentes", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),),
+            Text("Preços", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),),
+            Text("Comissão", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),),
             
 
             Expanded( //LINHA DEBAIXO
               child: Column(
               children: [
-              Padding(padding: EdgeInsets.only(top: 5)),
                 Divider(thickness: 2,color: Colors.black,),
-                Text('Resumo do mês'),
-                Text('Total de pedidos'),
-                Text('25'),
-                Text('Pedidos em aberto'),
-                Text('12'),
-                Text('Pedidos entregues'),
-                Text('13'),
+
+                Text('Resumo do mês', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w900, fontSize: 20),),
+                
+              Row(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                
+                  children: [
+                  
+                Column(
+                  children: [
+                      Text('Total de\npedidos', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),textAlign: TextAlign.center,),
+                      Text('25', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w900, fontSize: 50, color: Colors.green[200]),),
+                    ],  
+                  ),
+
+                Column(
+                    children: [
+                      Text('Pedidos em\naberto', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15), textAlign: TextAlign.center,),
+                      Text('12', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w900, fontSize: 50, color: Colors.green[200]),),
+                    ],
+                  ),
+                  
+                
+                Column(
+                  children: [
+                    Text('Pedidos\nentregues', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w500, fontSize: 15),textAlign: TextAlign.center,),
+                    Text('13', style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w900, fontSize: 50, color: Colors.green[200]),),
+                  ]
+              ),
+              ]),
               ],
+              
             )),
           ],
         ),
       ),
       ),
-      bottomNavigationBar: NavigationBar(
+      bottomNavigationBar: NavigationBar( //BARRA DE ESCOLHA
         destinations: [
-        NavigationDestination(icon: Icon(Icons.home), label: 'teste'),
-        NavigationDestination(icon: Icon(Icons.home), label: 'teste'),
+        NavigationDestination(icon: Image.asset('assets/images/inicio.png',width: 40), label: 'Inicio'),
+        NavigationDestination(icon: Image.asset('assets/images/cliente.png',width: 40), label: 'Cliente'),
+        NavigationDestination(icon: Image.asset('assets/images/pedidos.png',width: 40), label: 'Pedido'),
+        NavigationDestination(icon: Image.asset('assets/images/relatorio.png',width: 40), label: 'Relatório'),
       ]) ,
 
     );
